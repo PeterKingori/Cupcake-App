1886e31ff264a46084a400966aa7579e3e9db65c
Peter Kingori <kpndegwa@gmail.com>
2022-05-27 10:44:21 +0300

    Use listener binding to bind the button click listeners in the fragment classes to their respective layouts.

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -48,7 +48,7 @@ class PickupFragment : Fragment() {
         binding?.apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
-            nextButton.setOnClickListener { goToNextScreen() }
+            pickupFragment = this@PickupFragment
         }
     }
 
